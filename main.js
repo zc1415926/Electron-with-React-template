d5cdee9611e8867cc74deee260990d9a48c6b3eb
ZhangChuan <zc1415926@126.com>
2022-01-30 22:35:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const {app, BrowserWindow} = require('electron')
-const path = require('path')
 const isDev = require('electron-is-dev')
 const windowStateKeeper = require('electron-window-state');
 
